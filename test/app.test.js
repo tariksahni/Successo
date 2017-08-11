b4c94a29cd58b9174a68c25d1c189163a0ea3412
Tarik Sahni <tariksahni@gmail>
2017-08-11 22:05:50 +0530

    Testing Integrated

--- test/app.test.js
+++ test/app.test.js
@@ -5,19 +5,19 @@ global.render = render;
 global.mount = mount;
 import React from 'react';
 
-import SignIn from '../Components-Login/SignUp'
+import SignUp from '../Components-Login/SignUp'
 import Successo from '../App'
 import DoingBox from '../Components-DashBoard/DoingBox'
 
-describe('<Successo />', () => {
-    // it('it has a class signup_box', function () {
-    //     const wrapper = shallow(<SignIn/>);
-    //     expect(wrapper.find('.signup_box')).to.have.length(1);
-    // });
-    it('it contains DoingBox Component', function () {
-        const wrapper = mount(<Successo/>);
-        expect(wrapper.find(DoingBox)).to.have.length(1);
+describe('<SignIn />', () => {
+    it('it has a class signup_box', function () {
+        const wrapper = shallow(<SignUp/>);
+        expect(wrapper.find('.signup_box')).to.have.length(1);
     });
+    // it('it contains DoingBox Component', function () {
+    //     const wrapper = mount(<Successo/>);
+    //     expect(wrapper.find(DoingBox)).to.have.length(1);
+    // });
     // it('it contains DoneBox Component', function () {
     //     const wrapper = mount(<Successo/>);
     //     expect(wrapper.find(DoneBox)).to.have.length(1);
